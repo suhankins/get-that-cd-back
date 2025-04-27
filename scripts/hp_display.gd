4ebef3ee3db61eec5cfff0e0f102c9644b23ecdd
Dimitri Sukhankin <berna@detta.dev>
2025-04-28 01:12:48 +0400

    added missing hp display script

--- scripts/hp_display.gd
+++ scripts/hp_display.gd
@@ -1,5 +1,9 @@
 extends Label
 
 
+func _ready() -> void:
+	_on_player_health_updated(4)
+
+
 func _on_player_health_updated(health: int) -> void:
-	text = str(health) + "/4"
+	text = str(health + 1) + "/5"
